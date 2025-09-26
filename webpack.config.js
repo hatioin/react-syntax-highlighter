b4be87028b7f67dc5c28ea131753b49d3a4c6590
salahvi <salah@hatio.in>
2025-09-26 18:25:46 +0530

    Update Run Script

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,9 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /\.d\.ts$/ // ⬅ ignores TypeScript declaration files
     })
   ]
 };
